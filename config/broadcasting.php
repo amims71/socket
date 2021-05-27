e102c27150cf5080738159a33123c030b754c920
Amimul Ehshan <amimulehshan29@gmail.com>
2021-05-27 17:24:22 +0600

    pusher install

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -35,9 +35,16 @@ return [
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
+            /*'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
+            ],*/
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'encrypted' => false,
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http',
             ],
         ],
 
