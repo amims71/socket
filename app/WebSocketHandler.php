2cffeeea7ae144d1bc2239ae105e2a60e9430677
Amimul Ehshan <amimulehshan29@gmail.com>
2021-05-28 20:45:31 +0600

    from the library

--- app/WebSocketHandler.php
+++ app/WebSocketHandler.php
@@ -2,6 +2,16 @@
 
 namespace App;
 
+use BeyondCode\LaravelWebSockets\Apps\App;
+use BeyondCode\LaravelWebSockets\Dashboard\DashboardLogger;
+use BeyondCode\LaravelWebSockets\Facades\StatisticsLogger;
+use BeyondCode\LaravelWebSockets\QueryParameters;
+use BeyondCode\LaravelWebSockets\WebSockets\Channels\ChannelManager;
+use BeyondCode\LaravelWebSockets\WebSockets\Exceptions\ConnectionsOverCapacity;
+use BeyondCode\LaravelWebSockets\WebSockets\Exceptions\UnknownAppKey;
+use BeyondCode\LaravelWebSockets\WebSockets\Exceptions\WebSocketException;
+use BeyondCode\LaravelWebSockets\WebSockets\Messages\PusherMessageFactory;
+use Exception;
 use Ratchet\ConnectionInterface;
 use Ratchet\RFC6455\Messaging\MessageInterface;
 use Ratchet\WebSocket\MessageComponentInterface;
@@ -10,42 +20,113 @@ use function Ratchet\Client\connect;
 
 class WebSocketHandler implements MessageComponentInterface
 {
+    protected $channelManager;
+
+    public function __construct(ChannelManager $channelManager)
+    {
+        $this->channelManager = $channelManager;
+    }
     public function onOpen(ConnectionInterface $connection)
     {
-        // TODO: Implement onOpen() method.
+        $this
+            ->verifyAppKey($connection)
+            ->limitConcurrentConnections($connection)
+            ->generateSocketId($connection)
+            ->establishConnection($connection);
+        $channel=$this->channelManager->findOrCreate('12345','events');
+        $channel->subscribe($connection,new \stdClass());
+
         \Log::debug('ON OPEN');
-        $socketId = sprintf('%d.%d', random_int(1, 1000000000), random_int(1, 1000000000));
-        $connection->socketId = $socketId;
-        $connection->app = new \stdClass();
-        $connection->app->id = 'my_app';
     }
 
     public function onClose(ConnectionInterface $connection)
     {
-        // TODO: Implement onClose() method.
+        $this->channelManager->removeFromAllChannels($connection);
+
+        DashboardLogger::disconnection($connection);
+
+        StatisticsLogger::disconnection($connection);
         \Log::debug('ON CLOSE');
     }
 
-    public function onError(ConnectionInterface $connection, \Exception $e)
+    public function onError(ConnectionInterface $connection, Exception $exception)
     {
-        // TODO: Implement onError() method.
-        \Log::debug('ON ERROR');
-        // \Log::debug([$connection]);
-        // \Log::debug($e);
+        if ($exception instanceof WebSocketException) {
+            $connection->send(json_encode(
+                $exception->getPayload()
+            ));
+        }
+        \Log::debug('ON CLOSE');
     }
 
-    public function onMessage(ConnectionInterface $connection, MessageInterface $msg)
+    public function onMessage(ConnectionInterface $connection, MessageInterface $message)
     {
-        $connection->send('Hello World!');
-        // TODO: Implement onMessage() method.
-        \Log::debug(['ON MESSAGE', $msg]);
+        $message = PusherMessageFactory::createForMessage($message, $connection, $this->channelManager);
+
+        $message->respond();
+
+        StatisticsLogger::webSocketMessage($connection);
+        \Log::debug('ON MESSAGE');
     }
 
-    public function b(){
+   /* public function b(){
         \Ratchet\Client\connect('ws://localhost:6001/reader_stream')->then(function($conn) {
             $conn->send('hash updated!');
             $conn->close();
         }, function ($e) {});
        return true;
+    }*/
+
+
+
+    protected function verifyAppKey(ConnectionInterface $connection)
+    {
+        $appKey = '12345';
+
+        if (! $app = App::findByKey($appKey)) {
+            throw new UnknownAppKey($appKey);
+        }
+
+        $connection->app = $app;
+
+        return $this;
+    }
+
+    protected function limitConcurrentConnections(ConnectionInterface $connection)
+    {
+        if (! is_null($capacity = $connection->app->capacity)) {
+            $connectionsCount = $this->channelManager->getConnectionCount($connection->app->id);
+            if ($connectionsCount >= $capacity) {
+                throw new ConnectionsOverCapacity();
+            }
+        }
+
+        return $this;
+    }
+
+    protected function generateSocketId(ConnectionInterface $connection)
+    {
+        $socketId = sprintf('%d.%d', random_int(1, 1000000000), random_int(1, 1000000000));
+
+        $connection->socketId = $socketId;
+
+        return $this;
+    }
+
+    protected function establishConnection(ConnectionInterface $connection)
+    {
+        $connection->send(json_encode([
+            'event' => 'pusher:connection_established',
+            'data' => json_encode([
+                'socket_id' => $connection->socketId,
+                'activity_timeout' => 30,
+            ]),
+        ]));
+
+        DashboardLogger::connection($connection);
+
+        StatisticsLogger::connection($connection);
+
+        return $this;
     }
 }
